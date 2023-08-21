d21735545b25fb1205fee13322b7ceeee6aef914
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2023-08-21 15:44:35 -0400

    set log level to info

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,5 +1,5 @@
 bind = '0.0.0.0:5000'
-loglevel = 'debug'
+loglevel = 'info'
 accesslog = '/var/log/gunicorn_access.log'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
 errorlog =  '/var/log/gunicorn_error.log'
