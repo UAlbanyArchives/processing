5ce4729b5f15747107453de144bfca84ef7ee163
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2023-09-12 14:36:44 -0400

    allowed multiple threads?

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,5 +1,6 @@
 bind = '0.0.0.0:5000'
 loglevel = 'info'
+threads = 4
 accesslog = '/var/log/gunicorn_access.log'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
 errorlog =  '/var/log/gunicorn_error.log'
