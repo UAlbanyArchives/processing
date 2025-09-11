7c50e7289ea6418a423d6b4c727eb691daad305e
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-09-11 14:09:32 -0400

    added reindex script that logs

---
+++ reindex.py
@@ -0,0 +1,16 @@
+import os
+from datetime import datetime
+from iiiflow import collections, index_hocr_to_solr
+
+log_path = "/media/Library/SPE_Automated/reindex.log"
+
+def log(message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(log_path, "a", encoding="utf-8") as f:
+        f.write(f"[{timestamp}] {message}\n")
+
+for collection in collections:
+	log(collection.id)
+	for object_id in collection.objects:
+		log(f"\t{object_id}")
+		index_hocr_to_solr(collection.id, object_id)
